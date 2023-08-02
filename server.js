a6da94d89cddd1e412be531eb2621b2f70796a1f
sarevir0 <sarevir0@gmail.com>
2023-08-02 13:34:26 +0200

    70. Atlas Account

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import express from "express";
 const app = express();
 import morgan from "morgan";
 
+//routers
+import jobRouter from "./routes/jobRouter.js";
+
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
@@ -17,6 +20,17 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   console.log(req);
   res.json({ message: "data received", data: req.body });
+});
+
+app.use("/api/v1/jobs", jobRouter);
+
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: "not found" });
+});
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: "something went wrong" });
 });
 
 const port = process.env.PORT || 5100;
