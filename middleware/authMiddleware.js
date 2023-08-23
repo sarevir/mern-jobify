9ae3edbade552441f1e0e5359c59a173f53abf0f
sarevir0 <sarevir0@gmail.com>
2023-08-23 10:08:18 +0200

    103. Verify Cookie

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,4 +1,7 @@
+import { UnauthenticatedError } from "../errors/customErrors.js";
+
 export const authenticateUser = async (req, res, next) => {
-  console.log(req.cookies);
+  const { token } = req.cookies;
+  if (!token) throw new UnauthenticatedError("authentication invalid");
   next();
 };
