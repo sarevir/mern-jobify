9a134a483b080f3e5a5f2c8023d73f64634b2c06
sarevir0 <sarevir0@gmail.com>
2023-08-23 20:07:00 +0200

    114. Axios

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import customFetch from "./utils/customFetch.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const resp = await customFetch.get("/test");
+console.log(resp);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
